18c72f236e377b56f8f80a5f6636378878afff2f
hhnssl <losnah5h@gmail.com>
2024-03-08 17:14:25 +0900

    first commit

---
+++ READ.md
@@ -0,0 +1,11 @@
+# 아폴로 서버 v4
+
+https://www.apollographql.com/docs/apollo-server/getting-started
+
+# 실행 방법
+
+npm start
+
+# 코드 샌드 박스 버전
+
+https://codesandbox.io/s/github/apollographql/docs-examples/tree/main/apollo-server/v4/getting-started?fontsize=14&hidenavigation=1&theme=dark
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// 스키마
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+// 데이터셋(하드코딩)
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+// 리졸버
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
